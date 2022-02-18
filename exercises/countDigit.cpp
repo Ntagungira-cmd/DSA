e6337bacff75a44bfe3c85bb31959bc4e9fcb308
Ntagungira-cmd <ntagungiraali@gmail.com>
2022-02-18 06:13:42 +0200

    exercises

--- exercises/countDigit.cpp
+++ exercises/countDigit.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //Write 	a function named countDigit 	
 //that returns the number of times that 
-//a given digit appears in a 	positive number. 
+//a given digit appears in a positive number. 
 //For example countDigit(32121, 1) would return 2 	
 //because there are two 1s in 32121. Other examples:
 
@@ -10,8 +10,22 @@ using namespace std;
 //countDigit(33331, 6) returns 0
 //countDigit(3, 3) returns 1
 
-
+int countDigit(long long int num, int token){
+    if(num<0){
+      return -1;
+    }
+    string str=to_string(num);
+    string str2=to_string(token);
+    int size=str.length();
+    int appearanceCount=0;
+    for(int i=0;i<size; i++){
+       if(str[i]==str2[0]){
+           appearanceCount=appearanceCount+1;
+       }
+    }
+    return appearanceCount;
+}
 
 int main(){
-
+    cout<<countDigit(-33331, 3)<<endl;
 }
--- exercises/tempCodeRunnerFile.cpp
+++ exercises/tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-int arr[]= {21, 3, 7, 9, 11, 6};
-  // isMagicArray(arr,6);
+ // cout<<appearanceCount<<endl;
