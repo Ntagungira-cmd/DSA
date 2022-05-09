8e9571cb15afc22d99f333e0009ed539f45c96c3
ntagungira-cmd <ntagungiraali@gmail.com>
2022-05-09 11:55:05 +0200

    graph operations

--- DataStructures/Graph/adjacency_matrix.cpp
+++ DataStructures/Graph/adjacency_matrix.cpp
@@ -9,24 +9,47 @@ class adjMatrix{
 
     public:
     adjMatrix(int nodes){
-    this->nodes=nodes;
-    matrix=new int *[nodes];
-    for (int i = 0; i < nodes; i++) {
-      matrix[i] = new int[nodes];
-      for (int j = 0; j < nodes; j++)
-        matrix[i][j] = 0;
+        this->nodes=nodes;
+        matrix=new int *[nodes];
+        for (int i = 0; i < nodes; i++) {
+            matrix[i] = new int[nodes];
+            for (int j = 0; j < nodes; j++)
+                matrix[i][j] = 0;
+        }
     }
+    void display(){
+        for(int i=0;i<nodes; i++){
+            for(int j=0; j<nodes; j++)
+                cout<<matrix[i][j]<<"   ";
+            cout<<""<<endl;
+        }
     }
-void display(){
-    for(int i=0;i<nodes; i++){
-        for(int j=0; j<nodes; j++)
-            cout<<matrix[i][j]<<"   ";
-        cout<<""<<endl;
-    }
-}
+    void addEdge(int origin, int dest) {
+        if(origin<0||dest<0||origin>nodes||dest>nodes){
+          cout<<"Invalid node"<<endl;
+          return;
+        }
+        matrix[origin][dest] = 1;
+        // matrix[dest][origin] = 1;
+  }
+
+  void removeEdge(int origin, int dest) {
+        if(origin<0||dest<0||origin>nodes||dest>nodes){
+          cout<<"Invalid node"<<endl;
+          return;
+        }
+        matrix[origin][dest] = 0;
+        // matrix[dest][origin] = 0;
+  }
 };
 
 int main() {
   adjMatrix graph(4);
+  graph.display();
+  cout<<"add edge to graph"<<endl;
+  graph.addEdge(1,1);
+  graph.display();
+  cout<<"remove edge to graph"<<endl;
+  graph.removeEdge(1,1);
   graph.display();
 }
